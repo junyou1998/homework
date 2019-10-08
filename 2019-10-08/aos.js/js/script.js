659d1ccbc9dba4f32f5fbdfaff9808e8d3f83ace
陳俊佑 <35156817+junyou1998@users.noreply.github.com>
2019-10-08 21:32:33 +0800

    aos

---
+++ 2019-10-08/aos.js/js/script.js
@@ -0,0 +1,10 @@
+
+// jquery
+// $('.fa-bars').click(function(){
+//         $('.menu').toggleClass('active')  
+// })
+
+function togglePhoneNav(){
+    var nav = document.querySelector('#menu')
+    nav.classList.toggle('active')
+}
