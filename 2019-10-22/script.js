0c8fbd999cfd8e34fcc1a0df308fee93f06d8a32
junyou1998 <junyou1998@gmail.com>
2019-10-23 16:11:38 +0800

    todolist

---
+++ 2019-10-22/script.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    grabCursor: true,
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+})
