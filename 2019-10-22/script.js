f72c18cc42a9a4a18fc8ef76a637adbd5dcbd2e4
junyou1998 <junyou1998@gmail.com>
2019-10-25 16:13:53 +0800

    todolist

--- 2019-10-22/script.js
+++ 2019-10-22/script.js
@@ -10,18 +10,37 @@ var mySwiper = new Swiper('.swiper-container', {
 
 })
 
+
+
 $('.add_btn').click(function(){
     $('.add_panel').toggleClass('extend')
+    $('.add_btn').toggleClass('rotate')
     $('.mask').toggleClass('active')
 })
+
 $('.cancel_btn').click(function(){
     var btn_index = $('.cancel_btn').index(this)
 
-    $('.slide_area').removeClass('slide_control')
-
-    console.log(btn_index)
-    $(`.todo:nth-child(${btn_index + 1}) .slide_area`).toggleClass('slide_control')
+    
+    
+    if ($(`.todo:nth-child(${btn_index + 1}) .slide_area`).hasClass('slide_control')){
+        
+        $(`.todo:nth-child(${btn_index + 1}) .slide_area`).toggleClass('slide_control')
+        $(`.todo:nth-child(${btn_index + 1}) .cancel_btn`).toggleClass('rotate')
+    }
+    else{
+        $('.slide_area').removeClass('slide_control')
+        $('.cancel_btn').removeClass('rotate')
+        console.log(btn_index)
+        $(`.todo:nth-child(${btn_index + 1}) .slide_area`).toggleClass('slide_control')
+        $(`.todo:nth-child(${btn_index + 1}) .cancel_btn`).toggleClass('rotate')
+    }
+    
+        
 })
 $('.mask').click(function(){
-    console.log('dfdf');
+    console.log('dfdf')
+    $('.add_panel').toggleClass('extend')
+        $('.mask').toggleClass('active')
+    $('.add_btn').toggleClass('rotate')
 })
