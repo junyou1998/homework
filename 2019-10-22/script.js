11cafb45634533bdae1c5ae38b5b1736b43f3a62
junyou1998 <junyou1998@gmail.com>
2019-10-25 09:13:34 +0800

    todoliat

--- 2019-10-22/script.js
+++ 2019-10-22/script.js
@@ -6,5 +6,22 @@ var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'vertical',
     loop: true,
+    mousewheel: true,
 
+})
+
+$('.add_btn').click(function(){
+    $('.add_panel').toggleClass('extend')
+    $('.mask').toggleClass('active')
+})
+$('.cancel_btn').click(function(){
+    var btn_index = $('.cancel_btn').index(this)
+
+    $('.slide_area').removeClass('slide_control')
+
+    console.log(btn_index)
+    $(`.todo:nth-child(${btn_index + 1}) .slide_area`).toggleClass('slide_control')
+})
+$('.mask').click(function(){
+    console.log('dfdf');
 })
